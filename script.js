61237de39d25ca8b63c3ccdcca3618c578070ad4
Achilova-arch <asalachilova96@gmail.com>
2025-07-05 11:20:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const odam={
+    name:null,
+    id:null,
+    password:null
+}
+console.log(odam);
+const ism=prompt("Ism kiriting");
+const id=prompt("Id kiriting");
+const parol=prompt("parol kiriting");
+odam.name=ism;
+odam.id=id;
+odam.password=parol;
+console.log(odam);
